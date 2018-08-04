ac3a8800fb3a87c27c44dd135537ccdb138c3f2e
duvallee <duvallee@nate.com>
2018-08-04 21:07:58 +0900

    modified project settings

---
+++ Src/common/scheduler.c
@@ -0,0 +1,159 @@
+/*
+ * File: scheduler.c
+ *
+ * Written by duvallee in 2018
+ *
+*/
+#include "main.h"
+#include "string.h"
+#include "stm32f4xx_hal.h"
+#include "scheduler.h"
+
+
+#define MAX_SCHEDULER_COUNT                              1                          // max 8
+#define SCHEDULER_FLAG_SIZE                              32                         // does not modified
+
+#define NO_TIMER                                         0
+#define NO_EVENT                                         0
+
+#define RESET_EVENT_FLAG                                 1
+#define SET_EVENT_FLAG                                   1
+
+#define UNLOCK                                           0
+#define LOCK                                             1
+// ---------------------------------------------------------------------------
+typedef struct SOFT_TIMER_STRUCT
+{
+   uint32_t cycle_ms;                                    // 
+   uint32_t elapse_ms;
+   uint8_t expire_count;
+   volatile uint8_t lock;
+
+   TIMER_FN fn;
+} SOFT_TIMER;
+
+typedef struct WAIT_EVENT_STRUCT
+{
+   uint8_t event;
+
+   volatile uint8_t event_flag;
+   volatile uint8_t duplicate_event_flag;
+   volatile uint8_t lock;
+
+   uint8_t status;
+   WAIT_EVENT_FN fn;
+
+   uint16_t wait_ms;
+   uint32_t elapse_ms;
+} WAIT_EVENT;
+
+typedef struct SCHEDULER_STRUCT
+{
+   uint32_t sheduler_time_ms;
+
+   uint32_t soft_timer_flag[MAX_SCHEDULER_COUNT];
+   uint32_t event_flag[MAX_SCHEDULER_COUNT];
+
+   SOFT_TIMER soft_timer[MAX_SCHEDULER_COUNT * SCHEDULER_FLAG_SIZE];
+   WAIT_EVENT event[MAX_SCHEDULER_COUNT * SCHEDULER_FLAG_SIZE];
+} SCHEDULER;
+
+static SCHEDULER g_scheduler;
+
+// ***************************************************************************
+// Fuction      : HAL_SYSTICK_Callback()
+// Description  : 
+// 
+//
+// ***************************************************************************
+void HAL_SYSTICK_Callback()
+{
+   uint32_t index                                        = 0;
+   uint32_t bits                                         = 0;
+   unsigned char i;
+   g_scheduler.sheduler_time_ms++;
+
+   for (i = 0; i < (MAX_SCHEDULER_COUNT * SCHEDULER_FLAG_SIZE); i++)
+   {
+      if (g_scheduler.soft_timer[i].cycle_ms != 0)
+      {
+         if (g_scheduler.soft_timer[i].cycle_ms <= g_scheduler.soft_timer[i].elapse_ms)
+         {
+             g_scheduler.soft_timer[i].elapse_ms         = 1;
+            index                                        = i / SCHEDULER_FLAG_SIZE;
+            bits                                         = i % SCHEDULER_FLAG_SIZE;
+            g_scheduler.soft_timer_flag[index]           |= (0x1 << bits);
+         }
+         else
+         {
+             g_scheduler.soft_timer[i].elapse_ms++;
+         }
+      }
+      if (g_scheduler.event[i].event != NO_EVENT)
+      {
+         if (g_scheduler.event[i].event_flag == SET_EVENT_FLAG)
+         {
+            if (g_scheduler.event[i].wait_ms != 0)
+            {
+               if (g_scheduler.event[i].wait_ms <= g_scheduler.event[i].elapse_ms)
+               {
+                  g_scheduler.event[i].elapse_ms         = 1;
+                  index                                  = i / SCHEDULER_FLAG_SIZE;
+                  bits                                   = i % SCHEDULER_FLAG_SIZE;
+                  g_scheduler.event_flag[index]          |= (0x1 << bits);
+               }
+            }
+         }
+       }
+   }
+
+}
+
+// ***************************************************************************
+// Fuction      : scheduler_run()
+// Description  : 
+// 
+//
+// ***************************************************************************
+void scheduler_run()
+{
+   uint32_t index                                        = 0;
+   uint32_t bits                                         = 0;
+   unsigned char i;
+   for (i = 0; i < (MAX_SCHEDULER_COUNT * SCHEDULER_FLAG_SIZE); i++)
+   {
+      index                                              = i / SCHEDULER_FLAG_SIZE;
+      bits                                               = i % SCHEDULER_FLAG_SIZE;
+      if ((g_scheduler.event_flag[index] & (0x1 << bits)) != 0)
+      {
+         // call
+         g_scheduler.event_flag[index]                   &= ~(0x1 << bits);
+      }
+      if ((g_scheduler.soft_timer_flag[index] & (0x1 << bits)) != 0)
+      {
+         // call
+         g_scheduler.soft_timer_flag[index]              &= ~(0x1 << bits);
+      }
+   }
+}
+
+
+
+
+// ***************************************************************************
+// Fuction      : scheduler_init()
+// Description  : 
+// 
+//
+// ***************************************************************************
+void scheduler_init()
+{
+   memset(&g_scheduler, 0, sizeof(g_scheduler));
+}
+
+
+
+
+
+
+
--- Src/main.c
+++ Src/main.c
@@ -46,14 +46,10 @@
   ******************************************************************************
   */
 
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f4xx_hal.h"
 #include "usb_device.h"
-
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
+#include "scheduler.h"
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
@@ -67,12 +63,7 @@ TIM_HandleTypeDef htim9;
 
 UART_HandleTypeDef huart1;
 
-/* USER CODE BEGIN PV */
-/* Private variables ---------------------------------------------------------*/
 
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_ADC1_Init(void);
@@ -84,122 +75,99 @@ static void MX_TIM9_Init(void);
 static void MX_USART1_UART_Init(void);
 
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
-                                
 
-/* USER CODE BEGIN PFP */
-/* Private function prototypes -----------------------------------------------*/
-
-/* USER CODE END PFP */
-
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
+// ***************************************************************************
+// Fuction      : main()
+// Description  : 
+// 
+//
+// ***************************************************************************
 int main(void)
 {
+   HAL_Init();
+   scheduler_init();
+   SystemClock_Config();
 
-  /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
 
-  /* MCU Configuration----------------------------------------------------------*/
+   MX_GPIO_Init();
+   MX_ADC1_Init();
+   MX_SPI1_Init();
+   MX_SPI2_Init();
+   MX_TIM2_Init();
+   MX_TIM4_Init();
+   MX_TIM9_Init();
+   MX_USART1_UART_Init();
+   MX_USB_DEVICE_Init();
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_ADC1_Init();
-  MX_SPI1_Init();
-  MX_SPI2_Init();
-  MX_TIM2_Init();
-  MX_TIM4_Init();
-  MX_TIM9_Init();
-  MX_USART1_UART_Init();
-  MX_USB_DEVICE_Init();
-
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-  /* USER CODE END WHILE */
-
-  /* USER CODE BEGIN 3 */
-
-  }
-  /* USER CODE END 3 */
-
+   while (1)
+   {
+      scheduler_run();
+   }
 }
 
-/** System Clock Configuration
-*/
+// ***************************************************************************
+// Fuction      : SystemClock_Config()
+// Description  : 
+// 
+//
+// ***************************************************************************
 void SystemClock_Config(void)
 {
+   RCC_OscInitTypeDef RCC_OscInitStruct;
+   RCC_ClkInitTypeDef RCC_ClkInitStruct;
 
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+   // Configure the main internal regulator output voltage 
+   __HAL_RCC_PWR_CLK_ENABLE();
+   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
-    /**Configure the main internal regulator output voltage 
-    */
-  __HAL_RCC_PWR_CLK_ENABLE();
+   // Initializes the CPU, AHB and APB busses clocks 
+   RCC_OscInitStruct.OscillatorType                      = RCC_OSCILLATORTYPE_HSE;
+   RCC_OscInitStruct.HSEState                            = RCC_HSE_ON;
+   RCC_OscInitStruct.PLL.PLLState                        = RCC_PLL_ON;
+   RCC_OscInitStruct.PLL.PLLSource                       = RCC_PLLSOURCE_HSE;
+   RCC_OscInitStruct.PLL.PLLM                            = 16;
+   RCC_OscInitStruct.PLL.PLLN                            = 336;
+   RCC_OscInitStruct.PLL.PLLP                            = RCC_PLLP_DIV4;
+   RCC_OscInitStruct.PLL.PLLQ                            = 7;
+   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+   {
+      _Error_Handler(__FILE__, __LINE__);
+   }
 
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+   // Initializes the CPU, AHB and APB busses clocks 
+   RCC_ClkInitStruct.ClockType                           = RCC_CLOCKTYPE_HCLK       |
+                                                           RCC_CLOCKTYPE_SYSCLK     |
+                                                           RCC_CLOCKTYPE_PCLK1      |
+                                                           RCC_CLOCKTYPE_PCLK2;
+   RCC_ClkInitStruct.SYSCLKSource                        = RCC_SYSCLKSOURCE_PLLCLK;
+   RCC_ClkInitStruct.AHBCLKDivider                       = RCC_SYSCLK_DIV1;
+   RCC_ClkInitStruct.APB1CLKDivider                      = RCC_HCLK_DIV2;
+   RCC_ClkInitStruct.APB2CLKDivider                      = RCC_HCLK_DIV1;
 
-    /**Initializes the CPU, AHB and APB busses clocks 
-    */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+   {
+      _Error_Handler(__FILE__, __LINE__);
+   }
 
-    /**Initializes the CPU, AHB and APB busses clocks 
-    */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+   // Configure the Systick interrupt time 
+   HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+   // Configure the Systick 
+   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
-    /**Configure the Systick interrupt time 
-    */
-  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
-
-    /**Configure the Systick 
-    */
-  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-
-  /* SysTick_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+   // SysTick_IRQn interrupt configuration
+   HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
+
+
+
+
+
+
+
+
 
 /* ADC1 init function */
 static void MX_ADC1_Init(void)
@@ -529,51 +497,30 @@ static void MX_GPIO_Init(void)
 
 }
 
-/* USER CODE BEGIN 4 */
 
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @param  None
-  * @retval None
-  */
+// ***************************************************************************
+// Fuction      : _Error_Handler()
+// Description  : 
+// 
+//
+// ***************************************************************************
 void _Error_Handler(char * file, int line)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  while(1) 
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */ 
+   while(1) 
+   {
+   }
 }
 
 #ifdef USE_FULL_ASSERT
 
-/**
-   * @brief Reports the name of the source file and the source line number
-   * where the assert_param error has occurred.
-   * @param file: pointer to the source file name
-   * @param line: assert_param error line source number
-   * @retval None
-   */
+// ***************************************************************************
+// Fuction      : _Error_Handler()
+// Description  : 
+// 
+//
+// ***************************************************************************
 void assert_failed(uint8_t* file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-
 }
-
 #endif
 
-/**
-  * @}
-  */ 
-
-/**
-  * @}
-*/ 
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/

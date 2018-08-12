0d89a1412e425a469552d1156a0456feb6549157
duvallee <duvallee@nate.com>
2018-08-12 21:59:30 +0900

    add battery device driver

---
+++ Src/common/battery_gauge.c
@@ -0,0 +1,111 @@
+/*
+ * File: battery_gauge.c
+ *
+ * Written by duvallee in 2018
+ *
+*/
+#include "main.h"
+#include "battery_gauge.h"
+
+
+// ---------------------------------------------------------------------------
+#define BATTERY_GAUGE_TIMER                              5000
+
+#define BATTERY_MAX_ADC_VALUE                            65535
+#define BATTERY_MAX_MILI_VOLTAGE                         4300
+
+// ---------------------------------------------------------------------------
+static ADC_HandleTypeDef g_adc_port1;
+
+
+
+/* --------------------------------------------------------------------------
+ * Name : get_battery_voltage()
+ *
+ *
+ * -------------------------------------------------------------------------- */
+uint8_t get_battery_voltage(uint32_t* p_bat_voltage)
+{
+   uint32_t adc_value;
+   if (HAL_ADC_Start(&g_adc_port1) != HAL_OK)
+   {
+      debug_output_error("HAL_ADC_Start() failed !!! \r\n");
+      return ((uint8_t) -1);
+   }
+   if (HAL_ADC_PollForConversion(&g_adc_port1, 10) != HAL_OK)
+   {
+      debug_output_error("HAL_ADC_PollForConversion() failed !!! \r\n");
+      return ((uint8_t) -1);
+   }
+   adc_value                                             = HAL_ADC_GetValue(&g_adc_port1);
+   if (HAL_ADC_Stop(&g_adc_port1) != HAL_OK)
+   {
+      debug_output_error("HAL_ADC_Stop() failed !!! \r\n");
+      return ((uint8_t) -1);
+   }
+
+   // MAX Voltage for ADC = (BATTERY_DOWN_REGISTER / (BATTERY_DOWN_REGISTER + BATTERY_UP_REGISTER)) * 4.3V (max voltage of battery)
+   //                     = 3.3 V
+   //                 ADC = 65535 (16 bits)
+   // Current Voltage     = (adc value * BATTERY_MAX_MILI_VOLTAGE) / BATTERY_MAX_ADC_VALUE
+
+   *p_bat_voltage                                        = (adc_value * BATTERY_MAX_MILI_VOLTAGE) / BATTERY_MAX_ADC_VALUE;
+   return 0;
+}
+
+
+
+// ***************************************************************************
+// Fuction      : Battery_Gauge_Timer()
+// Description  : 
+// 
+//
+// ***************************************************************************
+void Battery_Gauge_Timer(uint32_t system_ms)
+{
+   uint32_t voltage_value                                = 0;
+   get_battery_voltage(&voltage_value);
+   debug_output_info("%d, %d mV \r\n", system_ms, voltage_value);
+}
+
+
+// ***************************************************************************
+// Fuction      : Battery_Gauge_Init()
+// Description  : 
+// 
+//
+// ***************************************************************************
+void Battery_Gauge_Init(void)
+{
+   ADC_ChannelConfTypeDef sConfig;
+
+   // Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
+   g_adc_port1.Instance                                  = ADC1;
+   g_adc_port1.Init.ClockPrescaler                       = ADC_CLOCK_SYNC_PCLK_DIV4;
+   g_adc_port1.Init.Resolution                           = ADC_RESOLUTION_12B;
+   g_adc_port1.Init.ScanConvMode                         = DISABLE;
+   g_adc_port1.Init.ContinuousConvMode                   = DISABLE;
+   g_adc_port1.Init.DiscontinuousConvMode                = DISABLE;
+   g_adc_port1.Init.ExternalTrigConvEdge                 = ADC_EXTERNALTRIGCONVEDGE_NONE;
+   g_adc_port1.Init.ExternalTrigConv                     = ADC_SOFTWARE_START;
+   g_adc_port1.Init.DataAlign                            = ADC_DATAALIGN_RIGHT;
+   g_adc_port1.Init.NbrOfConversion                      = 1;
+   g_adc_port1.Init.DMAContinuousRequests                = DISABLE;
+   g_adc_port1.Init.EOCSelection                         = ADC_EOC_SINGLE_CONV;
+   if (HAL_ADC_Init(&g_adc_port1) != HAL_OK)
+   {
+      _Error_Handler(__FILE__, __LINE__);
+   }
+
+   // Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+   sConfig.Channel                                       = ADC_CHANNEL_9;
+   sConfig.Rank                                          = 1;
+   sConfig.SamplingTime                                  = ADC_SAMPLETIME_3CYCLES;
+   if (HAL_ADC_ConfigChannel(&g_adc_port1, &sConfig) != HAL_OK)
+   {
+      _Error_Handler(__FILE__, __LINE__);
+   }
+
+   add_timer(BATTERY_GAUGE_TIMER, 0, Battery_Gauge_Timer);
+}
+
--- Src/main.c
+++ Src/main.c
@@ -68,10 +68,6 @@ int _putc(unsigned char ch)
 #endif
 }
 
-
-
-
-ADC_HandleTypeDef hadc1;
 SPI_HandleTypeDef hspi1;
 SPI_HandleTypeDef hspi2;
 TIM_HandleTypeDef htim2;
@@ -113,6 +109,10 @@ int main(void)
    debug_output_info("=============================================== \r\n\r\n");
 
    scheduler_init();
+
+   //
+   Battery_Gauge_Init();
+
 
 //   MX_GPIO_Init();
 //   MX_ADC1_Init();
@@ -198,42 +198,7 @@ void SystemClock_Config(void)
 
 #if 0
 
-/* ADC1 init function */
-static void MX_ADC1_Init(void)
-{
 
-  ADC_ChannelConfTypeDef sConfig;
-
-    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
-    */
-  hadc1.Instance = ADC1;
-  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
-  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = DISABLE;
-  hadc1.Init.ContinuousConvMode = DISABLE;
-  hadc1.Init.DiscontinuousConvMode = DISABLE;
-  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
-  hadc1.Init.DMAContinuousRequests = DISABLE;
-  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-  if (HAL_ADC_Init(&hadc1) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
-
-    /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
-    */
-  sConfig.Channel = ADC_CHANNEL_9;
-  sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
-
-}
 
 /* SPI1 init function */
 static void MX_SPI1_Init(void)
